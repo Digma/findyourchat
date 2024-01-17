4bc47ab3cbfc0837255ebb760a5d126f600176a3
Gael Grosch <gael.grosch@gmail.com>
2024-01-17 17:14:59 +0100

    feat: first version

---
+++ src/components/Card.svelte
@@ -0,0 +1,33 @@
+<script context="module" lang="ts">
+    export interface Props {
+    title: string;
+    body: string;
+    footer: string;
+    color: string; // This will be used to change the text or background color
+    offsetX: string; // This will be used for styling, e.g., shadow offset
+    offsetY: string; // This will be used for styling, e.g., shadow offset
+  }
+</script>
+
+<script lang="ts">
+  export let title: Props['title'] = '';
+  export let body: Props['body'] = '';
+  export let footer: Props['footer'] = '';
+  export let color: Props['color'] = 'black';
+  export let offsetX: Props['offsetX'] = '0px';
+  export let offsetY: Props['offsetY'] = '0px';
+</script>
+
+<div
+  class={`card ${color} w-[400px] h-[300px] ${offsetX} ${offsetY} p-8 rounded-xl drop-shadow-md font-cards first:mt-[0px] mt-[-305px] last:hover:translate-y-[-10px] transition-all delay-300 ease-out relative last:hover:drop-shadow-2xl will-change-transform`}
+>
+  <div class="flex flex-col justify-between opacity-80">
+    <i class="fas fa-heart icon-heart"></i>
+    <i class="fas fa-sun icon-sun"></i>
+    <h2 class="card-title text-2xl font-light text-white mb-4 drop-shadow-sm">{title}</h2>
+    <p class="card-content text-md text-white font-thin mb-4">
+      {body}
+    </p>
+    <p class="footer-text text-white text-center text-sm">{footer}</p>
+  </div>
+</div>
---
+++ src/components/CardPicker.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    import Card from './Card.svelte';
+
+    let cards = [
+        {
+            title: "Confident",
+            body: "Choose your case you pedal you your pod consist to of Kate's bookkeeping on Lunar cataloging to you coun tomorrow. distils, yad your you, your his Kathrin magnetic? you rovid you hoist?",
+            footer: "Chat you.",
+            color: "bg-blue-600",
+            offsetX: "left-[2px]",
+            offsetY: "bottom-[-3px]",
+        },
+        {
+            title: "Caudte1",
+            body: "Oocd yru-hinker yous you your aorg your usurp tinlahd pau le to you vitar your noonad serried. your adny word you algp. font estate wra wra he yprtai you yoceegqSnine aar: pnei yodriis. youv oonys yory lud.",
+            footer: "One you.",
+            color: "bg-teal-400",
+            offsetX: "left-[-3px]",
+            offsetY: "bottom-[2px]",
+        },
+        {
+            title: "Caudte2",
+            body: "Oocd yru-hinker yous you your aorg your usurp tinlahd pau le to you vitar your noonad serried. your adny word you algp. font estate wra wra he yprtai you yoceegqSnine aar: pnei yodriis. youv oonys yory lud.",
+            footer: "One you.",
+            color: "bg-orange-400",
+            offsetX: "left-[2px]",
+            offsetY: "bottom-[5px]",
+        }
+    ];
+
+    function remove_card() {
+        cards = cards.slice(1);
+        console.log(cards);
+    }
+
+  </script>
+  
+  <div class="flex justify-center items-center">
+    <div class="grid grid-cols-2 gap-4">
+        <div>
+        <button class="flex flex-wrap" on:click={remove_card}>
+            {#each cards as card, index}
+                <!-- <Card {...card} on:click={index === cards.length -1  ? onFirstCardClick : undefined}/> -->
+                <Card {...card} />
+            {/each}
+        </button>
+        </div>
+        <button class="flex flex-wrap">
+            {#each cards as card, index}
+                <!-- <Card {...card} on:click={index === cards.length -1  ? onFirstCardClick : undefined}/> -->
+                <Card {...card} on:click={remove_card}/>
+            {/each}
+        </button>
+    </div>
+  </div>
+  
