c1ba8fa32b008ca06ed4999067408e050e662142
Gael Grosch <gael.grosch@gmail.com>
2024-01-29 15:52:09 +0100

    feat: final design

--- src/components/cards/Card.svelte
+++ src/components/cards/Card.svelte
@@ -1,6 +1,7 @@
 <script context="module" lang="ts">
     export interface Props {
     title: string;
+    icon_src: string;
     body: string;
     footer: string;
     color: string; // This will be used to change the text or background color
@@ -11,21 +12,22 @@
 
 <script lang="ts">
   export let title: Props['title'] = '';
+  export let icon_src: Props['icon_src'] = '';
   export let body: Props['body'] = '';
   export let footer: Props['footer'] = '';
   export let color: Props['color'] = 'black';
+  export let textColor: Props['color'] = 'white';
   export let offsetX: Props['offsetX'] = '0px';
   export let offsetY: Props['offsetY'] = '0px';
 </script>
 
 <div
-  class={`card ${color} w-[400px] h-[300px] ${offsetX} ${offsetY} p-8 rounded-xl drop-shadow-md font-cards first:mt-[0px] mt-[-305px] last:hover:translate-y-[-10px] transition-all delay-300 ease-out relative last:hover:drop-shadow-2xl will-change-transform`}
+  class={`card ${color} w-[400px] h-[300px] ${offsetX} ${offsetY} p-8 rounded-xl drop-shadow-md font-cards first:mt-[0px] mt-[-305px] last:hover:translate-y-[-10px] transition-all delay-100 ease-out relative last:hover:drop-shadow-2xl will-change-transform`}
 >
-  <div class="flex flex-col justify-between opacity-80">
-    <i class="fas fa-heart icon-heart"></i>
-    <i class="fas fa-sun icon-sun"></i>
-    <h2 class="card-title text-2xl font-light text-white mb-4 drop-shadow-sm">{title}</h2>
-    <p class="card-content text-md text-white font-thin mb-4">
+  <div class="flex flex-col justify-between items-center">
+    <img src={icon_src} alt="card icon" class="w-[80px] h-[80px] mb-4" />
+    <h2 class={`card-title text-xl md:text-3xl font-bold text-${textColor} mb-4 drop-shadow-sm`}>{title}</h2>
+    <p class={`card-content text-md text-${textColor} font-thin mb-4`}>
       {body}
     </p>
     <p class="footer-text text-white text-center text-sm">{footer}</p>
---
+++ src/components/cards/CardPicker.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+    import Card from "./Card.svelte";
+    import iconConfident from '../../assets/card_icons/pink_yellow_4.svg';
+    import iconSimple from '../../assets/card_icons/purple_yellow_1.svg';
+
+    let cards_stack_1 = [
+        {
+            title: "Confident",
+            icon_src: iconConfident.src,
+            body: "Your writing exudes self-assuredness and conviction",
+            footer: "",
+            color: "bg-blue-accent",
+            offsetX: "left-[2px]",
+            offsetY: "bottom-[-3px]",
+        },
+        {
+            title: "Simple Language",
+            icon_src: iconConfident.src,
+            body: "You communicate concisely, making ideas accessible.",
+            footer: "",
+            color: "bg-lightblue-accent",
+            offsetX: "left-[-3px]",
+            offsetY: "bottom-[-1px]",
+        },
+        {
+            title: "Rich Sentences",
+            icon_src: iconConfident.src,
+            body: "Your writing is adorned with complexity, depth, and vividness.",
+            footer: "",
+            color: "bg-yellow-accent",
+            offsetX: "left-[2px]",
+            offsetY: "bottom-[2px]",
+        },
+        {
+            title: "Passionate",
+            icon_src: iconConfident.src,
+            body: "Your words resonate with emotion and conviction.",
+            footer: "",
+            color: "bg-purple-accent",
+            offsetX: "left-[2px]",
+            offsetY: "bottom-[5px]",
+        },
+    ];
+
+    let cards_stack_2 = [
+        {
+            title: "Cautious",
+            icon_src: iconSimple.src,
+            body: "Your writing reflects careful thought and meticulous attention to detail.",
+            footer: "",
+            color: "bg-blue-600",
+            offsetX: "left-[2px]",
+            offsetY: "bottom-[-3px]",
+        },
+        {
+            title: "Rich Vocabulary",
+            icon_src: iconSimple.src,
+            body: "Your writing is adorned with diverse and vivid language.",
+            footer: "",
+            color: "bg-yellow-accent",
+            offsetX: "left-[-3px]",
+            offsetY: "bottom-[-1px]",
+        },
+        {
+            title: "Short Sentences",
+            icon_src: iconSimple.src,
+            body: "You convey ideas crisply and succinctly, favoring brevity.",
+            footer: "",
+            color: "bg-blue-accent",
+            offsetX: "left-[3px]",
+            offsetY: "bottom-[3px]",
+        },
+        {
+            title: "Analytical",
+            icon_src: iconSimple.src,
+            body: "Characterized by logic, precision, and systematic thinking.",
+            footer: "",
+            color: "bg-blue-accent",
+            offsetX: "left-[2px]",
+            offsetY: "bottom-[5px]",
+        },
+    ];
+</script>
+
+<div class="flex justify-center items-center">
+    <div class="grid grid-cols-2 gap-4">
+        <!-- Stack 1 -->
+        <button
+            class="flex flex-wrap"
+        >
+            {#each cards_stack_1 as card}
+                <Card {...card} />
+            {/each}
+        </button>
+        <!-- Stack 2 -->
+        <button
+            class="flex flex-wrap"
+        >
+            {#each cards_stack_2 as card}
+                <Card {...card} />
+            {/each}
+        </button>
+    </div>
+</div>
