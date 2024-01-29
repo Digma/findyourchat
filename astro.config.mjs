2889d85f64435bc746c5ef2a58bd2bd7d084d063
Gael Grosch <gael.grosch@gmail.com>
2024-01-29 16:23:20 +0100

    feat: added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  renderers: [
-    '@astrojs/renderer-svelte',
-  ]
+  renderers: ['@astrojs/renderer-svelte'],
+  output: "server",
+  adapter: vercel()
 });
