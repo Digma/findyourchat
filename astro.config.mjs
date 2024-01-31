3b75756dfc559a72afffe306e1bafc235316cec1
Gael Grosch <gael.grosch@gmail.com>
2024-01-31 12:32:37 +0100

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), sitemap()],
   renderers: ['@astrojs/renderer-svelte'],
+  site: 'https://www.findyourchat.ai',
   output: "hybrid",
   adapter: vercel({
-    webAnalytics : { enabled: true },
+    webAnalytics: {
+      enabled: true
+    }
   })
 });
