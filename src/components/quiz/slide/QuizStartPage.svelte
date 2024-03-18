5db5ac5ef34edebe62fbad9374e42a5c1e5b6110
Gael Grosch <gael.grosch@gmail.com>
2024-03-18 17:26:50 +0100

    feat: new use prompt button

--- src/components/quiz/slide/QuizStartPage.svelte
+++ src/components/quiz/slide/QuizStartPage.svelte
@@ -1,69 +1,69 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
+    import { createEventDispatcher } from "svelte";
 
-  export let isEdit = false;
-  const dispatch = createEventDispatcher();
+    export let isEdit = false;
+    const dispatch = createEventDispatcher();
 
-  const handleKeydown = (e: KeyboardEvent) => {
-    switch (e.key) {
-      case "Enter":
-        dispatchStart();
-        break;
-      default:
-        break;
-    }
-  };
+    const handleKeydown = (e: KeyboardEvent) => {
+        switch (e.key) {
+            case "Enter":
+                dispatchStart();
+                break;
+            default:
+                break;
+        }
+    };
 
-  const dispatchStart = () => {
-    dispatch("start");
-  };
+    const dispatchStart = () => {
+        dispatch("start");
+    };
 </script>
 
 <svelte:window on:keydown={handleKeydown} />
 <div class="h-screen flex flex-col items-center justify-center text-center p-4">
-  <div class="max-w-96 flex flex-col items-center justify-center text-black">
-    <div class="mb-8">
-      <img src="/logo.png" alt="Logo" class="mx-auto h-24" />
+    <div class="max-w-96 flex flex-col items-center justify-center text-black">
+        <div class="mb-8">
+            <img src="/logo.png" alt="Logo" class="mx-auto h-24" />
+        </div>
+
+        <!-- Clock icon SVG -->
+
+        <h1 class="text-2xl font-bold mb-4">Get your Writing Style</h1>
+        <p class="mb-8 text-black">
+            Through a quick quiz, we will determine your writing style and provide you with a
+            writing prompt.
+        </p>
+
+        <!-- Start button -->
+        <button
+            id="startButton"
+            class="min-w-72 bg-black text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline hover:bg-opacity-90"
+            type="button"
+            on:click={dispatchStart}
+        >
+            {#if isEdit}
+                Edit Profile
+            {:else}
+                Start
+            {/if}
+        </button>
+
+        <div class="flex mt-4 gap-2">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-4 w-4 mb-4 text-black"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 6v6m0 0-4 2M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2z"
+                ></path>
+            </svg>
+            <p class="text-xs cursor-pointer">Takes 4 minutes</p>
+        </div>
     </div>
-
-    <!-- Clock icon SVG -->
-
-    <h1 class="text-2xl font-bold mb-4">Get your Writing Style</h1>
-    <p class="mb-8 text-black">
-      Through a quick quiz, we will determine your writing style and provide you
-      with a writing prompt.
-    </p>
-
-    <!-- Start button -->
-    <button
-      id="startButton"
-      class="bg-black text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline hover:bg-opacity-90"
-      type="button"
-      on:click={dispatchStart}
-    >
-      {#if isEdit}
-        Edit Profile
-      {:else}
-        Start
-      {/if}
-    </button>
-
-    <div class="flex mt-4 gap-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-4 w-4 mb-4 text-black"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 6v6m0 0-4 2M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2z"
-        ></path>
-      </svg>
-      <p class="text-xs cursor-pointer">Takes 4 minutes</p>
-    </div>
-  </div>
 </div>
--- src/components/quiz/slide/QuizSubmitSlide.svelte
+++ src/components/quiz/slide/QuizSubmitSlide.svelte
@@ -1,69 +1,59 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
-  import type { Question } from "../../../lib/personality/types";
+    import { createEventDispatcher } from "svelte";
+    import type { Question } from "../../../lib/personality/types";
 
-  export let enableSubmit: boolean = true;
-  export let answers: Question[] = [];
-  export let englishAnswered: boolean = false;
-  export let resultUrl: string = "";
+    export let enableSubmit: boolean = true;
+    export let answers: Question[] = [];
+    export let englishAnswered: boolean = false;
+    export let resultUrl: string = "";
 
-  $: emptyAnswers = answers
-    .map((obj, idx) => ({ ...obj, index: idx }))
-    .filter((q) => !q.answer);
+    $: emptyAnswers = answers.map((obj, idx) => ({ ...obj, index: idx })).filter((q) => !q.answer);
 
-  // Dispatch to parent
-  const dispatch = createEventDispatcher();
+    // Dispatch to parent
+    const dispatch = createEventDispatcher();
 
-  const submitAnswers = () => {
-    dispatch("submittedAnswers");
-  };
+    const submitAnswers = () => {
+        dispatch("submittedAnswers");
+    };
 
-  const scrollBackToAnswer = (index: number) => () => {
-    dispatch("scrollPrev", index);
-  };
+    const scrollBackToAnswer = (index: number) => () => {
+        dispatch("scrollPrev", index);
+    };
 </script>
 
 <div class="w-full min-w-0 relative">
-  <div class="flex flex-col items-center m-32">
-    {#if enableSubmit}
-      <h2 class="text-3xl font-bold text-black mb-4">Submit your answers</h2>
-      <p class="text-lg text-black mb-4">
-        You have completed the quiz 🎉 If you are done, click on the Submit
-        button
-      </p>
-      <div class="rounded-md bg-black">
-        <a
-          class="btn text-white font-bold"
-          href={resultUrl}
-          on:click={submitAnswers}>Submit</a
-        >
-      </div>
-    {:else}
-      <h2 class="text-3xl font-bold text-black mb-4">
-        Please answer all questions
-      </h2>
-      <p class="text-lg text-black mb-4">
-        You must answer all questions before submitting.
-      </p>
-      <div class="flex flex-row flex-wrap justify-center">
-        {#each emptyAnswers as answer}
-          <div class="flex justify-between m-1">
-            <button
-              class="btn text-white font-bold bg-primary-500"
-              on:click={scrollBackToAnswer(answer.index)}>{answer.title}</button
-            >
-          </div>
-        {/each}
-        {#if !englishAnswered}
-          <div class="flex justify-between m-1">
-            <button
-              class="btn text-white font-bold bg-primary-500"
-              on:click={scrollBackToAnswer(answers.length)}
-              >"English Type"</button
-            >
-          </div>
+    <div class="flex flex-col items-center m-32">
+        {#if enableSubmit}
+            <h2 class="text-3xl font-bold text-black mb-4">Submit your answers</h2>
+            <p class="text-lg text-center italic text-black mb-4">
+                You have completed the quiz 🎉 <br /> If you are done, click on the Submit button
+            </p>
+            <div class="mt-4 min-w-72 text-center rounded-md bg-black">
+                <a class="btn text-white font-bold" href={resultUrl} on:click={submitAnswers}
+                    >Submit</a
+                >
+            </div>
+        {:else}
+            <h2 class="text-3xl font-bold text-black mb-4">Please answer all questions</h2>
+            <p class="text-lg text-black mb-4">You must answer all questions before submitting.</p>
+            <div class="flex flex-row flex-wrap justify-center">
+                {#each emptyAnswers as answer}
+                    <div class="flex justify-between m-1">
+                        <button
+                            class="btn text-white font-bold bg-primary-500"
+                            on:click={scrollBackToAnswer(answer.index)}>{answer.title}</button
+                        >
+                    </div>
+                {/each}
+                {#if !englishAnswered}
+                    <div class="flex justify-between m-1">
+                        <button
+                            class="btn text-white font-bold bg-primary-500"
+                            on:click={scrollBackToAnswer(answers.length)}>"English Type"</button
+                        >
+                    </div>
+                {/if}
+            </div>
         {/if}
-      </div>
-    {/if}
-  </div>
+    </div>
 </div>
